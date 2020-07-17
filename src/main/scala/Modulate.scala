4cf6eab75ef186934e4e77bce8e761655ff724e9
T. Alexander Popiel <tapopiel@gmail.com>
2020-07-17 11:26:28 -0700

    Use Nil instead of Unit

--- src/main/scala/Modulate.scala
+++ src/main/scala/Modulate.scala
@@ -2,7 +2,7 @@ import scala.math.BigInt
 
 object Modulate {
   def demodulate(s: String): (Any, String) = (s take 2) match {
-    case "00" => (Unit, s drop 2)
+    case "00" => (Nil, s drop 2)
     case "11" => {
       val (a, r1) = demodulate(s drop 2)
       val (b, r2) = demodulate(r1)
@@ -21,7 +21,7 @@ object Modulate {
   }
 
   def modulate(v: Any): String = v match {
-    case Unit => "00"
+    case Nil => "00"
     case (a: Any, b: Any) => "11" + modulate(a) + modulate(b)
     case n: BigInt => if (n > 0) {
       val l = n.bitLength + 3 / 4
